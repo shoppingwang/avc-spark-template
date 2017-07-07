b98c779499734f5d2ca4138ffdd8cf2438f898ab
王小平 <wangxiaoping@avc-mr.com>
2017-07-07 13:53:34 +0800

    修改示例工程代码结构

--- src/main/scala/com/avcdata/data/template/spark/streaming/processor/SampleProjectProcessor.scala
+++ src/main/scala/com/avcdata/data/template/spark/streaming/processor/SampleProjectProcessor.scala
@@ -1,12 +1,9 @@
 package com.avcdata.data.template.spark.streaming.processor
 
-import com.avcdata.etl.common.util.KafkaUtil
 import com.avcdata.etl.streaming.template.StreamProcessTemplate
 import com.avcdata.etl.streaming.util.KafkaStreamUtil
-import kafka.message.MessageAndMetadata
-import kafka.serializer.StringDecoder
 import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaUtils}
+import org.apache.spark.streaming.kafka.HasOffsetRanges
 
 /**
   * 示例工程处理器
@@ -23,38 +20,15 @@ class SampleProjectProcessor(batchDurationSeconds: Int, configItems: Map[String,
     */
   protected override def process(ssc: StreamingContext): Unit =
   {
-    //需要消费的topic
-    val topicsSet = configItems("kafka.read.topics").split(",").toSet
-
-    //Kafka的ZK地址
-    val zookeeperAddress = configItems("kafka.zookeeper.hosts")
-
     //topic消费信息
-    val commonConnectUri = configItems("data.common.db.connecturi")
+    val commonConnectURI = configItems("data.common.db.connecturi")
     val commonUsername = configItems("data.common.db.username")
     val commonPassword = configItems("data.common.db.password")
 
-    val fromOffsets = KafkaUtil.fromOffsets(commonConnectUri, commonUsername, commonPassword, topicsSet, zookeeperAddress)
-
-    // Create direct kafka stream with brokers and topics
-    val kafkaParams = Map[String, String](
-      "auto.offset.reset" -> configItems.getOrElse("kafka.auto.offset.reset", "largest"),
-      "zookeeper.connect" -> zookeeperAddress,
-      "group.id" -> "TimeHonoredBrandPosts",
-      "metadata.broker.list" -> KafkaUtil.getBrokerList(zookeeperAddress)
-    )
-
-    //获取流数据
-    val kafkaStream = fromOffsets.map
-    { fo =>
-
-      val messageHandler = (mmd: MessageAndMetadata[String, String]) => (mmd.key, mmd.message)
-
-      KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder, (String, String)](ssc, kafkaParams, fo, messageHandler)
-    } getOrElse KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-
     //主处理逻辑
-    kafkaStream.foreachRDD(jsonRDD =>
+    KafkaStreamUtil.createDirectStream(ssc, configItems("kafka.read.topics"), configItems("kafka.zookeeper.hosts")
+      , commonConnectURI, commonUsername, commonPassword, Map("fetch.message.max.bytes" -> String.valueOf(1024 * 1024 * 10))
+    ).foreachRDD(jsonRDD =>
     {
       //获取当前消费数据的偏移量信息
       val offsetRanges = jsonRDD.asInstanceOf[HasOffsetRanges].offsetRanges
@@ -64,7 +38,7 @@ class SampleProjectProcessor(batchDurationSeconds: Int, configItems: Map[String,
       jsonRDD.foreach(println)
 
       //更新数据库topic的offset值
-      KafkaStreamUtil.updateDBOffsets(commonConnectUri, commonUsername, commonPassword, offsetRanges)
+      KafkaStreamUtil.updateDBOffsets(commonConnectURI, commonUsername, commonPassword, offsetRanges)
     })
   }
 }
